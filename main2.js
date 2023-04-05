e831c4be4553c30b47198cf4cc7f0fb7fec2e6bc
Regis066 <mugisharegis72@gmail.com>
2023-04-05 14:43:07 +0200

    initial commit

--- main2.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {login,dashboard} = require('./starter/main')
-
-router.route('/dashboard').get(dashboard)
-router.route('/login').post(login)
-
-module.exports = router
- 
